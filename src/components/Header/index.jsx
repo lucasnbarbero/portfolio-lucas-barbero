bc9363aa2896c28f492f4df3a10a869de009b558
Lucas Barbero <lucasnbarbero95@gmail.com>
2023-02-18 11:09:36 -0300

    Mejoras en el responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,16 +9,19 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Lucas Barbero</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
-        <CTA />
+        <div className="header__content">
+          <div className="header__content-title">
+            <h5>Hello I'm</h5>
+            <h1>Lucas Barbero</h1>
+            <h5 className="text-light">Fullstack Developer</h5>
+            <CTA />
+          </div>
+          <div className="me">
+            <img src={ME} alt="" />
+          </div>
+        </div>
 
         <HeaderSocials />
-
-        <div className="me">
-          <img src={ME} alt="" />
-        </div>
 
         <a href="#contact" className="scroll__down">
           Scroll Down
